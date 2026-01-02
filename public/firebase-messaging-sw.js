ac705cc78f883a47430ebea90dbd1e6f7951e7fa
DevExcel Developer <114825217+talibabbas77@users.noreply.github.com>
2026-01-02 23:05:57 +0500

    place change

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 // Firebase Messaging Service Worker
 // This file is required for RichInfo push notifications (PubID: 997602, SiteID: 382124)
+// Must be accessible at: https://www.punkcikowo.pl/firebase-messaging-sw.js
 // eslint-disable-next-line no-undef
 importScripts('https://platformio-resources.s3.amazonaws.com/js/firebase-messaging-sw-remote.js');
- 
+
